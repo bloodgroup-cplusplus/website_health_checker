620d7f672c1d5c12476998fcfca236668580f1fd
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2023-10-07 22:57:06 +0530

    completed main.go

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main 
 import (
 	"github.com/urfave/cli/v2"
+	"fmt"
+	"os"
+	"log"
 )
 
 
@@ -8,8 +11,34 @@ func main(){
 	app := &cli.App{
 		Name: "Healthchecker",
 		Usage: "A tiny tool that checks whether a website is up and running or not "
-		Flags:
-		Action:
+		Flags: [] cli.Flag{
+			&cli.StringFlag{
+				Name:"domain",
+				Alises::[]string{"d"},
+				Usage:"Domain name to check.",
+				Required:true,
+			},
+			&cli.StringFlag{
+				Name:"port",
+				Aliases:[]string("p")
+				Usage:"Port number to check",
+				Requred:false,
+			},
+		Action: func (c *cli.Context) error {
+			port := c.String("port")
+			if c.String("port") == ""{
+				// the default port 
+				port = "80"
+			}
+			status := Check(c.String("domain"),port)// we send domain and port to check function present in check.go file
+			fmt.Println(status)
+			return nil
+		},
 	}
+	err := app.Run(os.Args)
+	if err !=nil {
+		log.Fatal(err)
+	}
+
 }
 
